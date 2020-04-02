cbf16d585481d2d32d0f5953cc7fa8abdd1e3bae
Vilém Zouhar <vilem.zouhar@gmail.com>
2020-04-02 16:52:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TNTranslator
+
+Translation Navigator Translator. Part of the N-best list navigator task at Bergamot.
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.ts',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  // mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'tntranslator-web.js',
+    path: path.resolve(__dirname, 'web')
+  }
+};
+
